5c7fe168f5d3ee4f661ba6731593edf823850e67
Steven Silvers <stevensilvers11@gmail.com>
2017-02-04 19:35:14 -0800

    file io working

--- obstaclealpha/Source.cpp
+++ obstaclealpha/Source.cpp
@@ -1,8 +1,8 @@
 //Alpha version of obstacle avoidance system for OSU ARLISS Project
 
-
+#include <string>
 #include <stdio.h>
-#include <string.h>
+
 #include <iostream>
 #include <fstream>
 const int Width = 25;
@@ -12,6 +12,7 @@ const int Height = 25;
 
 void Analyze(int(Array)[][Height], int rows, int cols) {
 
+	//prints the array
 	for (int x = 0; x < rows; x++) {
 		std::cout << std::endl;
 		for (int y = 0; y < cols; y++) {
@@ -19,10 +20,34 @@ void Analyze(int(Array)[][Height], int rows, int cols) {
 			std::cout << Array[x][y] << " ";
 		}
 	}
-			return;
-		}
+
+
+
+	return;
+}
 
 void main() {
+
+	//open file
+	
+	std::ifstream infile;
+
+
+	infile.open("test.txt");
+	if (infile.is_open()) {
+		std::cout << "file opened. \n";
+			for (unsigned i = 0; i < Width; i++)
+				for (unsigned j = 0; j < Height; j++)
+					//read in from file, delimited by , and /n to [i][j]
+					int k; // holder so it will compile
+		infile.close();
+	}
+	else {
+		std::cout << "file wasn't opened.\n";
+	}
+
+
+
 
 	int Image_Array[Width][Height];
 
